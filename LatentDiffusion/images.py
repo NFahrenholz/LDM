382932e5ed6bd10cc6a083a1850a1ca6781c6ebf
Nicolo Fahrenholz <nicolo.fahrenholz@stud.tu-darmstadt.de>
2023-07-21 08:43:25 +0200

    added script to visualize

--- LatentDiffusion/images.py
+++ LatentDiffusion/images.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 
 from PIL import Image, ImageDraw
@@ -6,7 +8,78 @@ from torchmetrics.image.fid import FrechetInceptionDistance
 from utils import *
 from config import config
 
-def launch(args):
+def launch_single(args):
+    device = config.device
+    if args.device is not None:
+        device = args.device
+
+    model, autoencoder, scheduler = get_conditional_model()
+    x_transformer = get_xtransformer()
+
+    if args.model is not None:
+        checkpoint = torch.load(args.model, map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+    scheduler.set_timesteps(args.timesteps)
+
+    # Create the initial noise to sample from
+    sample_shape = (1, config.ls_channels, config.ls_size, config.ls_size)
+    latents = torch.randn(sample_shape).to(device) * scheduler.init_noise_sigma
+
+    # Get the real Image
+    data = pd.read_csv(config.dataset_path)
+    real_image_path = random.choice(data["Image Path"])
+
+    # Create the unconditional condition
+    uncond_cond = torch.zeros((20, 5), dtype=torch.int)
+    uncond_cond[:] = torch.IntTensor([0, 0, 0, 0, 36])
+    uncond_cond = uncond_cond.flatten().to(device)
+
+    # read the image
+    img = cv2.imread(real_image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # get the bounding boxes
+    tree = ET.parse(f"../data/BoundingBoxes/{real_image_path.split('/')[-1]}.xml")
+    root = tree.getroot()
+
+    bboxes = get_bboxes(root)
+
+    # transform the image and the bounding boxes
+    transformed = config.transform(image=img, bboxes=bboxes)
+
+    # create the conditioning
+    cond = uncond_cond
+    cond[:len(transformed['bboxes'] * 5)] = torch.IntTensor(transformed['bboxes']).flatten()
+
+    # concat cond and uncond
+    uncond_cond = x_transformer(torch.unsqueeze(uncond_cond, 0))
+    cond = x_transformer(torch.unsqueeze(cond, 0))
+
+
+    for i, ts in enumerate(tqdm(scheduler.timesteps)):
+        inp = scheduler.scale_model_input(latents, ts)
+
+        with torch.no_grad():
+            u = model(inp, ts, uncond_cond).sample
+            t = model(inp, ts, cond, return_dict=False)[0]
+
+        pred = u + args.guidance_scale * (t - u)
+
+        latents = scheduler.step(pred, ts, latents).prev_sample
+
+    sample = latent_to_pil(latents, autoencoder)[0]
+
+    draw = ImageDraw.Draw(sample)
+    for bbox in transformed['bboxes']:
+        draw.rectangle(bbox[:-1], outline='blue')
+
+    grid = make_grid([sample, Image.fromarray(transformed['image'])], 1, 2)
+    test_dir = os.path.join(config.output_dir, "samples")
+    os.makedirs(test_dir, exist_ok=True)
+    grid.save(f"{test_dir}/grid.png")
+
+def launch_batch(args):
     device = config.device
     bs = args.batch_size
 
@@ -105,7 +178,9 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--model', action='store')
     parser.add_argument('-bs', '--batch_size', action='store', default=16, type=int)
     parser.add_argument('-t', '--timesteps', action='store', default=50, type=int)
+    parser.add_argument('-gs', '--guidance_scale', action='store', default=7.5, type=float)
+    parser.add_argument('-d', '--device', action='store')
 
     args = parser.parse_args()
 
-    launch(args)
+    launch_single(args)
